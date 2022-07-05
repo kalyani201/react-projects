ef95a2f9b0c2abed409cbc23f6465dd1c1be04e2
kalyani <kbarsaiyan@deqode.com>
2022-07-05 19:00:51 +0530

    refresh button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ function App() {
   //   const newTour = tour.filter((tour) => tour.id !== id)
   //   setTour(newTour);
   // } 
+  
+  // if(tour.length === 0) {
+  //   console.log(Tour)
+  //  return(
+    
+  //   <div>
+      
+  //     <h2>No data found</h2>
+  //     <button>Refresh</button>
+  //   </div>
+  //  )
+  // }
   return (
     <div className="App">
       <h3>Tour Project</h3>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
 
-const Card = ({tour, setTour}) => {
+const Card = ({ tour, setTour }) => {
     const [readmore, setReadmore] = useState(false);
     const removeTour = (id) => {
         const newTour = tour.filter((tour) => tour.id !== id)
         setTour(newTour);
+        console.log(newTour)
     }
-    return(
+    return (
         <div>
             {
-                tour.map( (tourInfo) => {
-                    const {id , name, info, image,price} = tourInfo
-                    return(
+                tour.map((tourInfo) => {
+                    const { id, name, info, image, price } = tourInfo
+                    return (
                         <div key={id} className="card">
-                            <img src={image} alt="image" width="100%"/>
+                            <img src={image} alt="image" width="100%" />
                             <div>
                                 <p><b>{name}</b></p>
                                 <p>{price}</p>
                             </div>
                             <p>
                                 {readmore ? info : `${info.substring(0, 120)}...`}
-                                <button onClick={()=> setReadmore(!readmore)}>{ readmore ? "Read Less" : "Read More"}</button>
+                                <button onClick={() => setReadmore(!readmore)}>{readmore ? "Read Less" : "Read More"}</button>
                             </p>
                             <button onClick={() => removeTour(id)}>Not Interested</button>
                         </div>
-                        
+
                     )
                 })
             }
-           
+
         </div>
     )
 }
