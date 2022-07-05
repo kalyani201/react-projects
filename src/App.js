16d28525abc5b3557bb44e4cf2061d8f2eb11d1a
kalyani <kbarsaiyan@deqode.com>
2022-07-05 17:40:30 +0530

    tour project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import data from './components/data';
+import Card from './components/Card';
 
 function App() {
-  
+  const [tour, setTour] = useState(data);
+  // const removeTour = (id) => {
+  //   const newTour = tour.filter((tour) => tour.id !== id)
+  //   setTour(newTour);
+  // } 
   return (
     <div className="App">
-      <h3>React Projects</h3>
+      <h3>Tour Project</h3>
+      <Card tour={tour} setTour={setTour} />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Card = ({tour, setTour}) => {
+    const [readmore, setReadmore] = useState(false);
+    const removeTour = (id) => {
+        const newTour = tour.filter((tour) => tour.id !== id)
+        setTour(newTour);
+    }
+    return(
+        <div>
+            {
+                tour.map( (tourInfo) => {
+                    const {id , name, info, image,price} = tourInfo
+                    return(
+                        <div key={id} className="card">
+                            <img src={image} alt="image" width="100%"/>
+                            <div>
+                                <p><b>{name}</b></p>
+                                <p>{price}</p>
+                            </div>
+                            <p>
+                                {readmore ? info : `${info.substring(0, 120)}...`}
+                                <button onClick={()=> setReadmore(!readmore)}>{ readmore ? "Read Less" : "Read More"}</button>
+                            </p>
+                            <button onClick={() => removeTour(id)}>Not Interested</button>
+                        </div>
+                        
+                    )
+                })
+            }
+           
+        </div>
+    )
+}
+
+export default Card
