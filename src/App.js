7713a4f086038f1d1b4d2f1fcb6128a27b5868e2
kalyani <kbarsaiyan@deqode.com>
2022-07-05 14:39:28 +0530

    node module

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h3>React Projects</h3>
+    </div>
+  );
+}
+
+export default App;
